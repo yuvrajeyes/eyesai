f097fd252d465a6d48e54708bfed1d5e3113a4d3
Yuvraj Singh <yuvrajeyes@gmail.com>
2021-10-23 21:31:38 +0530

    bug fixing

--- public/demo.js
+++ public/demo.js
@@ -14,41 +14,8 @@ authLink.addEventListener("click", function () {
   }
 });
 
-// User Details Edit/Update
-let userdetailsEdit = document.querySelector(".userdetailsEdit");
-let edit = document.querySelector(".edit");
-let editclass = userdetailsEdit.classList;
-
-edit.addEventListener("click", function () {
-  let input = userdetailsEdit.querySelectorAll("input");
-
-  let result = editclass.toggle("edit");
-
-  if (result) {
-    input.forEach((inp) => {
-      inp.disabled = false;
-    });
-    const btn = document.createElement('input');
-    btn.type = 'submit';
-    btn.value = 'Update';
-    btn.classList.add('updateUser');
-    console.log(btn);
-    userdetailsEdit.appendChild(btn);
-    edit.innerHTML = '<img scr="./images/left-arrow.png" alt="Back';
-  } else {
-    input.forEach((inp) => {
-      inp.disabled = true;
-    });
-    let update = document.querySelector('.updateUser');
-    update.parentElement.removeChild(update);
-    edit.innerHTML = '<span class="edit" style="cursor: pointer; text-decoration: underline;">Edit</span>';
-  }
-});
-
-
 // Delete single tasks
 const trashcans = document.querySelectorAll(".delete");
-
 trashcans.forEach((trashcan) => {
   trashcan.addEventListener("click", (e) => {
     const endpoint = `/tasks/${trashcan.dataset.doc}`;
